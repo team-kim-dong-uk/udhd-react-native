e64b63cee746a61d8512535a4cce65cd9802de02
dongjinBaek <dj9136@gmail.com>
2021-08-28 16:19:18 +0900

    base url .env 사용하도록 변경

--- src/api/photoAPI.js
+++ src/api/photoAPI.js
@@ -9,7 +9,7 @@ export const getPhotos = ({userId, findAfter}) => {
     if (findAfter) {
         query += `&findAfter=${findAfter}`;
     }
-    console.log(findAfter);
+    console.log(client.defaults.baseURL);
     console.log(query);
     return client.get(query);
 }
