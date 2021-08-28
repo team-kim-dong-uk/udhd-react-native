2c80562e81b08465e574985674d5b71bcdf96330
jooomOut <rimeilo324@naver.com>
2021-08-28 15:24:44 +0900

    feat: Header에 Logo, Icon 탭 위치 설정
    좌우 정렬

--- src/components/layout/UdhdHeader.js
+++ src/components/layout/UdhdHeader.js
@@ -1,45 +1,46 @@
 import React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
 import PhotoGrid from '../PhotoGrid';
-
+import {SearchIcon} from "native-base";
 
 const UdhdHeader = () => {
   return (
-    <View style={styles.header}>
-        {/*TODO ICON*/}
-        <Image
-            style={styles.tinyLogo}
-            source={{uri: "http://img.danawa.com/prod_img/500000/869/844/img/2844869_1.jpg?shrink=360:360&_v=20210325103140"}}/>
-        <View>
-            <Text style={styles.headerText}>Header~</Text>
+    <View style={styles.headerContainer}>
+        <Image style={styles.tinyLogo}
+               source={{uri: "http://img.danawa.com/prod_img/500000/869/844/img/2844869_1.jpg?shrink=360:360&_v=20210325103140"}}/>
+        {/*TODO Search Box*/}
+        <View style={styles.upperTap}>
+            <Image style={styles.upperIcon}
+                   source={{uri: "http://img.danawa.com/prod_img/500000/869/844/img/2844869_1.jpg?shrink=360:360&_v=20210325103140"}}/>
+            <Image style={styles.upperIcon}
+                   source={{uri: "http://img.danawa.com/prod_img/500000/869/844/img/2844869_1.jpg?shrink=360:360&_v=20210325103140"}}/>
         </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-    header: {
+    headerContainer: {
       width: '100%',
         height: '8%', /*이거 고정값으로 바꾸고 싶은데 뭔지 모르겠음*/
       flexDirection: 'row',
       alignItems: 'center',
-      justifyContent: 'center'
+      justifyContent: 'space-between',
     },
-    headerText: {
-      fontWeight: 'bold',
-      fontSize: 20,
-      color: '#333',
-      letterSpacing: 1
+    upperTap: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        marginRight: 20,
     },
     tinyLogo:{
       width: 50,
       height: 50,
+      marginLeft: 20,
+    },
+    upperIcon: {
+        width: 50,
+        height: 50,
     }
 
 });
