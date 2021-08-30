453aae075fe8c04abe57b31d1d46bfcab3ac9801
dongjinBaek <dj9136@gmail.com>
2021-08-30 14:37:09 +0900

    소셜로그인 callback 추가

--- src/components/LoginWebviewModal.js
+++ src/components/LoginWebviewModal.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import { Dimensions, StyleSheet } from 'react-native';
 import WebView from 'react-native-webview';
+import { API_URL } from '@env';
+import { loginSuccess } from '../core/redux/auth';
+import { useDispatch } from 'react-redux';
 
+const userAgent = `userAgent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'`;
+const INJECTED_JAVASCRIPT =
+    '(function() {if(window.document.getElementsByTagName("pre").length>0){window.ReactNativeWebView.postMessage((window.document.getElementsByTagName("pre")[0].innerHTML));}})();';
 const LoginWebviewModal = () => {
+  const dispatch = useDispatch();
+
+  
+  const _handleMessage = async event => {
+    console.log(event);
+    const data = JSON.parse(event.nativeEvent.data);
+    dispatch(loginSuccess(data));
+    // console.log(JSON.parse(event.nativeEvent.data));
+    // let result = JSON.parse(event.nativeEvent.data);
+    // let success = result.success;
+    // if (success) {
+    //   let userToken = result.userToken;gi
+    //   try {
+    //     await AsyncStorage.setItem('userToken', userToken);
+    //   } catch (e) {
+    //     console.log(e);
+    //   }
+    // }
+    // props.closeSocialModal();
+  };
 
   return (
     // <Modal>
@@ -31,7 +57,9 @@ const LoginWebviewModal = () => {
                 <button id='kakao-login' style='background-color:yellow'>
                   카카오 계정으로 로그인
                 </button>
-                <button id='google-login'>구글 계정으로 로그인</button>
+                <button id='google-login'>
+                  <a href='${API_URL}/oauth2/authorization/google'>구글 계정으로 로그인
+                </button>
                 <button id='apple-login' style='background-color:black;color:white'>
                   애플 계정으로 로그인
                 </button>
@@ -39,6 +67,11 @@ const LoginWebviewModal = () => {
             </div>
           </body>
           ` }}
+        userAgent={userAgent}
+        useWebKit={true}
+        javaScriptEnabled={true}
+        injectedJavaScript={INJECTED_JAVASCRIPT}
+        onMessage={_handleMessage}
         style={styles.webview}
       />
     // </Modal>
