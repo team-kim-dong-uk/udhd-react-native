ead29f00dea500bce9bdeca289d50ab2436e257b
jooomOut <rimeilo324@naver.com>
2021-09-16 21:23:05 +0900

    Feat: add header in photo detail screen

--- src/components/screen/PhotoScreen.js
+++ src/components/screen/PhotoScreen.js
@@ -2,20 +2,24 @@ import React, {useEffect, useState} from 'react';
 import {Image, Pressable, StyleSheet, Text, View} from 'react-native';
 import { Dimensions } from 'react-native';
 import {useNavigation} from "@react-navigation/native";
+import UdhdHeader from "../layout/UdhdHeader";
 
 const PhotoScreen = ({route}) => {
   console.log(route);
   const navigation = useNavigation();
   return (
-    <View style={styles.container}>
-      <Pressable onPress={() => navigation.goBack()}>
-        <Image
-            source={{uri: route.params.image}}
-            style={styles.photoContainer}
-        />
-      </Pressable>
-      {/*TODO 태그 리스트*/}
-    </View>
+      <View>
+        <UdhdHeader/>
+        <View style={styles.container}>
+          <Pressable onPress={() => navigation.goBack()}>
+            <Image
+                source={{uri: route.params.image}}
+                style={styles.photoContainer}
+            />
+          </Pressable>
+          {/*TODO 태그 리스트*/}
+        </View>
+      </View>
   );
 }
 
