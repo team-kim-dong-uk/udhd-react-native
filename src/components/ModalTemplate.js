85ad45599a906949f2afa8394693d5d886a69d0b
jooomOut <rimeilo324@naver.com>
2021-10-13 22:59:44 +0900

    Feat: 이미지 상세 설정 배경 반투명화

--- src/components/ModalTemplate.js
+++ src/components/ModalTemplate.js
@@ -10,14 +10,13 @@ const ModalTemplate = ({children, show, onControlModal, onShow, style}) => {
     return (
         <Modal
             style={[style, {margin: 0}]}
-            animationType="none"
+            animationType="slide"
             transparent={true}
             visible={show}
             onRequestClose={onControlModal}
             onBackdropPress={onControlModal}
             onShow={onShow}
             coverScreen={true}
-            style={{margin: 0}}
         >
             {children}
         </Modal>
