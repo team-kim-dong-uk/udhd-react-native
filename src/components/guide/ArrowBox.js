a17a2fc7c31a88c5e7e3018dc62632b7c61fc34f
dongjinBaek <dj9136@gmail.com>
2021-10-13 16:27:46 +0900

    feat: 여러 개의 가이드를 이어 볼 수 있는 기능 추가

--- src/components/guide/ArrowBox.js
+++ src/components/guide/ArrowBox.js
@@ -14,20 +14,43 @@ const ArrowBox = ({item}) => {
   };
 
   return (
-    <View style={[boxSize, styles.boxStyle]}>
-      <View style={[styles.arrowHead, {transform: [{rotate: "45deg"}]}]}></View>
+    <View style={[boxSize,
+                  styles.boxStyle,
+                  item.type.startsWith('bottom') ? styles.bottomBorder : null,
+                  item.type.startsWith('top') ? styles.topBorder : null,
+                  item.type.endsWith('left') ? styles.leftBorder : null,
+                  item.type.endsWith('right') ? styles.rightBorder : null,
+                  ]}>
+      <View style={[
+        styles.arrowHead,
+        item.type === 'bottom-right' ? styles.arrowHeadBottomRight : null,
+        item.type === 'bottom-left' ? styles.arrowHeadBottomLeft : null,
+        item.type === 'top-right' ? styles.arrowHeadTopRight : null,
+        item.type === 'top-left' ? styles.arrowHeadTopLeft : null,
+        {transform: [{rotate: "45deg"}]}
+        ]}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   boxStyle: {
-    borderBottomWidth: 2 * width,
-    borderRightWidth: 2 * width,
     borderRadius: 2 * width,
     borderColor: colors.orange,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bottomBorder: {
+    borderBottomWidth: 2 * width,
+  },
+  topBorder: {
+    borderTopWidth: 2 * width,
+  },
+  leftBorder: {
+    borderLeftWidth: 2 * width,
+  },
+  rightBorder: {
+    borderRightWidth: 2 * width,
   },
   textStyle: {
     fontFamily: fonts.NotoSansCJKkr,
@@ -39,15 +62,35 @@ const styles = StyleSheet.create({
     color: colors.white,
   },
   arrowHead: {
-    borderTopWidth: 2 * width,
-    borderLeftWidth: 2 * width,
     borderColor: colors.orange,
     width: 10 * width,
     height: 10 * width,
     position: 'absolute',
+  },
+  arrowHeadBottomRight: {
+    borderTopWidth: 2 * width,
+    borderLeftWidth: 2 * width,
     top: 0,
     right: -6 * width,
-  }
+  },
+  arrowHeadBottomLeft: {
+    borderTopWidth: 2 * width,
+    borderLeftWidth: 2 * width,
+    top: 0,
+    left: -6 * width,
+  },
+  arrowHeadTopRight: {
+    borderBottomWidth: 2 * width,
+    borderRightWidth: 2 * width,
+    bottom: 0,
+    right: -6 * width,
+  },
+  arrowHeadTopLeft: {
+    borderBottomWidth: 2 * width,
+    borderRightWidth: 2 * width,
+    bottom: 0,
+    left: -6 * width,
+  },
 });
 
 export default ArrowBox;
--- src/components/guide/HelpModal.js
+++ src/components/guide/HelpModal.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
 import {
+  Pressable,
     StyleSheet, Text, View,
 } from 'react-native';
 import { colors, fonts, height, width } from '../../util/StyleUtil';
@@ -11,6 +12,7 @@ import TextBox from './TextBox';
 
 const HelpModal = ({show, guides}) => {
   const [idx, setIdx] = useState(0);
+  const [showState, setShowState] = useState(show);
 
   guides = [{
     textBox: {
@@ -25,6 +27,7 @@ const HelpModal = ({show, guides}) => {
       height: 35,
       top: 64.5,
       left: 283,
+      type: 'bottom-right',
     },
     targetBox: {
       width: 40,
@@ -32,19 +35,71 @@ const HelpModal = ({show, guides}) => {
       top: 19,
       left: 313,
     },
-  }]
+  },{
+    textBox: {
+      width: 165,
+      height: 60,
+      top: 69.5,
+      left: 158,
+      text: '내 앨범 검색에서 내 사진들을 태그별로 모아 볼 수 있어요',
+    },
+    arrowBox: {
+      width: 50,
+      height: 35,
+      top: 70,
+      left: 108,
+      type: 'bottom-left',
+    },
+    targetBox: {
+      width: 220,
+      height: 40,
+      top: 19,
+      left: 50,
+    },
+  },{
+    textBox: {
+      width: 140,
+      height: 80,
+      top: 460,
+      left: 17,
+      text: '검색 메뉴에서 나에게 없는 사진만을 찾아보세요',
+    },
+    arrowBox: {
+      width: 20,
+      height: 60,
+      top: 490,
+      left: 157,
+      type: 'top-right',
+    },
+    targetBox: {
+      width: 50,
+      height: 50,
+      top: 560,
+      left: 155,
+    },
+  }
+];
+
+  const onGuidePress = (idx, guides) => {
+    if (idx === guides.length - 1) {
+      setShowState(false);
+    } else {
+      setIdx(idx + 1);
+    }
+  };
+
   return (
     <ModalTemplate
       style={styles.container}
-      show={show}
+      show={showState}
       onControlModal={()=>{}}
       onShow={()=>{}}
     >
-    <View style={styles.container}>
+    <Pressable style={styles.container} onPress={() => onGuidePress(idx, guides)}>
       <TextBox item={guides[idx].textBox}></TextBox>
       <ArrowBox item={guides[idx].arrowBox}></ArrowBox>
       <TargetBox item={guides[idx].targetBox}></TargetBox>
-    </View>
+    </Pressable>
     </ModalTemplate>
   );
 }
@@ -55,7 +110,7 @@ const styles = StyleSheet.create({
     height: 628 * height,
     marginTop:StatusBar.currentHeight,
     backgroundColor: colors.black,
-    opacity: 0.75,
+    opacity: 0.7,
     zIndex: 3,
   },
 });
