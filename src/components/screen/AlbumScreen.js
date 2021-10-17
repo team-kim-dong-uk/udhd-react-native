d9f955cb71b373f46adadc3395e4d59ac62b2416
dongjinBaek <dj9136@gmail.com>
2021-10-17 13:28:06 +0900

    feat: search screen에서도 guide가 최초 한번만 보이도록 수정

--- src/components/screen/AlbumScreen.js
+++ src/components/screen/AlbumScreen.js
@@ -7,26 +7,18 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 import { colors } from '../../util/StyleUtil';
 import AlbumHelpModal from '../guide/AlbumHelpModal';
+import { getShowGuideFromStorage } from '../../util/AsyncStorageUtil';
 
 const AlbumScreen = () => {
     const {searching, auth} = useSelector(state => state);
     const navigation = useNavigation();
     const [showGuide, setShowGuide] = useState(undefined);
-    const getShowGuideStorage = async (setState, type) => {
-      const val = await AsyncStorage.getItem(`showGuide.${type}`);
-      if (val === 'false') {
-        setState(false);
-      } else {
-        await AsyncStorage.setItem(`showGuide.${type}`, 'false')
-        setState(true);
-      }
-    }
     
     useEffect(()=>{
       if (auth.data.hasPhotos) {
         navigation.navigate('UploadSelect');
       } else if (showGuide === undefined) {
-        getShowGuideStorage(setShowGuide, 'album');
+        getShowGuideFromStorage(setShowGuide, 'album');
       }
     }, [auth, showGuide])
   return (
