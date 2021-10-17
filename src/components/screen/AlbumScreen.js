6ad24bbe1fcff4e00cf9954bd6a6f35ec1f17ea6
dongjinBaek <dj9136@gmail.com>
2021-10-17 13:15:44 +0900

    feat: album showGuide 여부를 async storage에 저장

--- src/components/screen/AlbumScreen.js
+++ src/components/screen/AlbumScreen.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import UdhdHeader from "../layout/UdhdHeader";
 import PhotoGrid from "../PhotoGrid";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 import { colors } from '../../util/StyleUtil';
 import AlbumHelpModal from '../guide/AlbumHelpModal';
@@ -10,14 +11,27 @@ import AlbumHelpModal from '../guide/AlbumHelpModal';
 const AlbumScreen = () => {
     const {searching, auth} = useSelector(state => state);
     const navigation = useNavigation();
+    const [showGuide, setShowGuide] = useState(undefined);
+    const getShowGuideStorage = async (setState, type) => {
+      const val = await AsyncStorage.getItem(`showGuide.${type}`);
+      if (val === 'false') {
+        setState(false);
+      } else {
+        await AsyncStorage.setItem(`showGuide.${type}`, 'false')
+        setState(true);
+      }
+    }
+    
     useEffect(()=>{
       if (auth.data.hasPhotos) {
         navigation.navigate('UploadSelect');
+      } else if (showGuide === undefined) {
+        getShowGuideStorage(setShowGuide, 'album');
       }
-    })
+    }, [auth, showGuide])
   return (
       <View style={styles.container}>
-        <AlbumHelpModal show={true}/>
+        {showGuide && <AlbumHelpModal show={true}/>}
         <UdhdHeader type='album'/>
           {!searching.data && <PhotoGrid type='album'/>}
       </View>
